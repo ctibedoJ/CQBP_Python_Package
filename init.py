8830b562b32d0f48b55241715e029679128a741d
ctibedoJ <ctibedo@gmail.com>
2025-08-08 12:13:24 -0400

    Create init.py

---
+++ init.py
@@ -0,0 +1,8 @@
+""" Classical Quantum Biophysics (CQBP) Python Package
+A comprehensive framework for modeling biological systems using principles from classical and quantum physics, with applications in drug discovery and biophysical modeling. """
+version = '0.1.0'
+from .core import *
+from .fields import *
+from .quantum import *
+from .molecular import *
+from .applications import *
